c402073ea9f23ed30463fb5484da71d07aee9543
Prokhor Proshkin <pproshkin@yandex.ru>
2021-03-23 20:01:01 +0300

    !!!

--- Parcer_lamoda.py
+++ Parcer_lamoda.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 
 
 
-res = requests.get("https://www.lamoda.ru/c/33/shoes-tufli")
+res = requests.get("https://www.lamoda.ru/c/33/")
 
 soup = BeautifulSoup(res.text, 'html.parser')
-for item in soup.select(".products-list-item_labels"):
+for item in soup.select(".products-list-item"):
 
-    print(item.text)
+    print(item.attrs["data-price"])
+    print(item.div.attrs["data-brand"])
+    print(item.div.attrs["data-name"])
+
+
 
 
